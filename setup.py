69442932461d2ad957727e52c6786593259f2b1a
Frank Kiwy <54667906+fkiwy@users.noreply.github.com>
2024-06-14 13:10:46 +0200

    Update setup version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='unTimely_Catalog_explorer',
-      version='1.1.0',
+      version='1.2.0',
       description='A search and visualization tool for the unTimely Catalog',
       url='https://github.com/fkiwy/unTimely_Catalog_explorer',
       author='Frank Kiwy',
